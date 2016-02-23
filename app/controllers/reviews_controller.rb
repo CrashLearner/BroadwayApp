220e277aa2db5ce23ee44036d8fe965713ff3c1e
CrashLearner <yonayen@hotmail.com>
2016-02-23 15:18:16 -0800

    Edit and Destroy Actions with View File Changes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 	before_action :find_play
+	before_action :find_review, only: [:edit, :update, :destroy]
 
 	def new
 		@review = Review.new
@@ -17,6 +18,22 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update	
+		if @review.update(review_params)
+			redirect_to play_path(@play)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to play_path(@play)
+	end
+
 	private 
 
 		def review_params
@@ -25,6 +42,10 @@ class ReviewsController < ApplicationController
 
 		def find_play
 			@play = Play.find(params[:play_id])
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
 		end
 
 end
